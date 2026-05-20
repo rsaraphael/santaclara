85640f63fdf085ffc405e62abd0674176ca43b40
Raphael Alves <raphaelalves@bfaindustries.com>
2026-05-20 15:26:56 -0300

    Add ponte de venda

--- quermesse-caixa/vite.config.js
+++ quermesse-caixa/vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-products-json',
+      writeBundle() {
+        copyFileSync(
+          resolve(__dirname, 'src/products.json'),
+          resolve(__dirname, 'd/products.json')
+        )
+      },
+    },
+  ],
   base: '',
   build: {
     outDir: 'd',
+    copyPublicDir: false,
   },
 })
